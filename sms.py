0fea6d4823e13ab562cfb4449ae320e33e2bab78
Paulo Carrasco González <pcarrasco@173.1.0.54.reverse.gogrid.com>
2018-03-15 16:15:06 -0300

    Se agregar argumentos

--- sms.py
+++ sms.py
@@ -4,4 +4,5 @@ sns = boto3.client('sns')
 numero = sys.argv[1]   #'+56992229813'
 mensaje = sys.argv[2]
 sns.publish(PhoneNumber = numero, Message=mensaje)
-#print number
+print numero
+print mensaje
