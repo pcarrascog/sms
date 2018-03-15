88ccd50a11dbac93195626c9b4a635934d70062f
Paulo Carrasco González <pcarrasco@173.1.0.54.reverse.gogrid.com>
2018-03-15 16:10:37 -0300

    Se agregar argumentos

--- sms.py
+++ sms.py
@@ -1,6 +1,6 @@
 import sys
 import boto3
 sns = boto3.client('sns')
-number = sys.argv   #'+56992229813'
+number = sys.argv[1]   #'+56992229813'
 #sns.publish(PhoneNumber = number, Message='Error en procedimiento' )
 print number
